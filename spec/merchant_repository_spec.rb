7c30f61cce036300f5969fd50b5da223534718ea
Phil McCarthy <philmcc2631@gmail.com>
2021-06-01 15:12:19 -0600

    Add merchant repo test cases; implement find_all_by_name

--- spec/merchant_repository_spec.rb
+++ spec/merchant_repository_spec.rb
@@ -16,19 +16,67 @@ RSpec.describe MerchantRepository do
       expect(@mr.all.size).to eq(9)
     end
 
-    it '#find_by_id' do
-      merchant = @mr.find_by_id(12334115)
+    context '#find_by_id' do
+      it 'finds an existing merchant by id' do
+        merchant = @mr.find_by_id(12334115)
 
-      expect(merchant).to be_an_instance_of Merchant
-      expect(merchant.id).to eq(@merchants_data[3][:id].to_i)
-      expect(merchant.name).to eq(@merchants_data[3][:name])
+        expect(merchant).to be_an_instance_of Merchant
+        expect(merchant.id).to eq(@merchants_data[3][:id].to_i)
+        expect(merchant.name).to eq(@merchants_data[3][:name])
+      end
+
+      it 'returns nil if no matching merchant exists' do
+        nil_merchant = @mr.find_by_id(777)
+  
+        expect(nil_merchant).to be_nil
+      end
     end
 
-    it '#find_by_name' do
-      merchant = @mr.find_by_name('LolaMarleys')
+    context '#find_by_name' do
+      it 'returns a single merchant with matching name' do
+        merchant = @mr.find_by_name('LolaMarleys')
 
-      expect(merchant).to be_an Merchant
-      expect(merchant.name).to eq('LolaMarleys')
+        expect(merchant).to be_a Merchant
+        expect(merchant.name).to eq('LolaMarleys')
+      end
+      
+      it 'matches merchant name case insensitively' do
+        merchant = @mr.find_by_name('LOLamarleYS')
+  
+        expect(merchant).to be_a Merchant
+        expect(merchant.name).to eq('LolaMarleys')
+      end
+
+      it 'returns nil if no matching merchant exists' do
+        nil_merchant = @mr.find_by_name('zzz')
+  
+        expect(nil_merchant).to be_nil
+      end
+    end
+
+    context '#find_all_by_name' do
+      it 'returns an array of merchants with name match fragments' do
+        merchants = @mr.find_all_by_name('in')
+  
+        expect(merchants).to be_an Array
+        expect(merchants.size).to eq(2)
+        expect(merchants.first).to be_a Merchant
+        expect(merchants.first.name).to include('in')
+        expect(merchants.last.name).to include('in')
+      end
+      
+      it 'returns matches case insensitively' do
+        merchants = @mr.find_all_by_name('IN')
+  
+        expect(merchants).to be_an Array
+        expect(merchants.size).to eq(2)
+      end
+
+      it 'return an empty array when no matches exist' do
+        no_merchants_match = @mr.find_all_by_name('zzz')
+
+        expect(no_merchants_match).to eq([])
+      end
     end
   end
 end
