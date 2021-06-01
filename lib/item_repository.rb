c82a9b80cfe647e486a009a9289627beb1dbfa4d
Phil McCarthy <philmcc2631@gmail.com>
2021-06-01 14:54:45 -0600

    Override #inspect

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -3,10 +3,14 @@ require_relative 'repositable'
 
 class ItemRepository
   include Repositable
-  
+
   attr_reader :all
 
   def initialize(data)
     @all = data.map { |item_data| Item.new(item_data) }  
+  end
+
+  def inspect
+    "#<#{self.class} #{@items.size} rows>"
   end
 end
--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -4,10 +4,14 @@ require_relative 'repositable'
 
 class MerchantRepository
   include Repositable
-  
+
   attr_reader :all
 
   def initialize(data)
     @all = data.map { |merchant_data| Merchant.new(merchant_data) }
+  end
+
+  def inspect
+    "#<#{self.class} #{@merchants.size} rows>"
   end
 end
