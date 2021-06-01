7e0f2cc5e1fdd44b9886cdac61943e9bf0258030
Phil McCarthy <philmcc2631@gmail.com>
2021-06-01 10:26:17 -0600

    Add spec for merchant repo spec, add fixture file

---
+++ spec/merchant_repository_spec.rb
@@ -0,0 +1,17 @@
+require 'csv'
+require './lib/merchant_repository'
+
+RSpec.describe MerchantRepository do
+  describe 'class methods' do
+    it 'initializes with all merchants loaded as objects' do
+      merchants_data = CSV.read('./spec/fixtures/merchants_fixture.csv', headers: true, header_converters: :symbol)
+
+      mr = MerchantRepository.new(merchants_data)
+
+      expect(mr).to be_an_instance_of MerchantRepository
+      expect(mr.merchants).to be_an Array
+      expect(mr.merchants.first).to be_an_instance_of Merchant
+      expect(mr.merchants.size).to eq(9)
+    end
+  end
+end
